3936651708690450c8c6707d7f630b3b1b80aae7
dasha-alekseenkova <alekseenkovadash@gmail.com>
2022-10-05 03:17:08 +0200

    Commit

--- hang.rb
+++ hang.rb
@@ -1,74 +1,39 @@
 class Hanging
   def initialize
-    @word = words.sample
-    @lives = 7
-    @word_teaser = ""
-    @word.first.size.times do
-      @word_teaser += "_ "
-    end
+    @word = "incomprehensibility"
   end
 
-  def words
-    [
-      ["pizza", "The best food ever"],
-      ["princess", "From royal family, but not a queen "],
-      ["mother", "She gave you life"],
-      ["preparation", "When you are getting ready for smth"],
-      ["acceleration", "Rate at which velocity changes with time"],
-    ]
+  def hide_word
+    @user_word = @word.gsub(/\w/, "_")
+    puts @user_word
   end
 
-  def print_teaser last_guess = nil
-    update_teaser(last_guess) unless last_guess.nil?
-    puts @word_teaser
+  def guess_word
+    puts "Guess a letter"
+    @guess = gets.chomp
   end
 
-  def update_teaser last_guess
-    new_teaser = @word_teaser.split
-    new_teaser.each_with_index do |letter, index|
-      if letter == '_' && @word.first[index] == last_guess
-        new_teaser[index] = last_guess
+  def check_if_matches
+    index = 0
+    @word.each_char do |letter|
+      if letter == @guess
+        @user_word[index] = letter
       end
+      index += 1
     end
-    @word_teaser = new_teaser.join(' ')
+    puts @user_word
   end
 
-  def make_guess
-    if @lives > 0
-      puts "Enter a letter"
-      guess = gets.chomp
-      good_guess = @word.first.include? guess
-      if guess == "exit"
-        puts "Thank you for playing!"
-      elsif guess.length > 1
-        puts "only guess 1 letter at a time please!"
-        make_guess
-      elsif good_guess
-        puts "You are correct!"
-        print_teaser guess
-        if @word.first == @word_teaser.split.join
-          puts "YEES you win"
-        else
-          make_guess
-        end
-      else
-        @lives -= 1
-        puts "Sorry... you have #{ @lives } lives left. Try again!"
-        make_guess
-      end
-    else
-      puts "You lost,sorry"
+  def play_game
+    hide_word
+    while @user_word != @word
+      guess_word
+      check_if_matches
     end
-  end
-
-def begin
-    puts "New game started... your word is #{ @word.first.size } characters long"
-    puts "To exit game at any point type 'exit'"
-    print_teaser
-    puts "Clue: #{ @word.last }"
-    make_guess
   end
 end
 
-game = Hanging.new
-game.begin
+game = Hangman.new
+game.play_game
+@word = "incomprehensibility"
+puts" Congratulations!! You won this game, the hidden word is #{@word}"
